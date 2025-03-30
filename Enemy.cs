1ff04ede532a43cc172773acb5586e224d81b8df
Twoods22-web <85687213+Twoods22-web@users.noreply.github.com>
2025-03-30 18:30:26 -0400

    Game manager and enemy files updated with enemy two

--- Enemy.cs
+++ Enemy.cs
@@ -13,10 +13,16 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f);
-        if (transform.position.y < -6.5f)
+        transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 3f); // Move the enemy downwards at a speed of 3 units per second
+
+        if(transform.position.y < -6.5f)
         {
-            Destroy(this.gameObject);
+            Destroy(this.gameObject); // Destroy the enemy if it goes below the screen (y < -6.5)
+        }
+        else if (transform.position.x < -9f || transform.position.x > 9f)
+        {
+            Destroy(this.gameObject); // Destroy the enemy if it goes out of the horizontal bounds (x < -9 or x > 9)
+            // This ensures that enemies do not stay in the scene indefinitely
         }
     }
 }
