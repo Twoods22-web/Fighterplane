915d820a308c6fb0fc53f35431b49297470e316e
Twoods22-web <85687213+Twoods22-web@users.noreply.github.com>
2025-03-30 21:30:47 -0400

    Enemy2 code

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(-1, 0, 0) * Time.deltaTime * 3f); // Move the enemy downwards at a speed of 3 units per second
+
+        if (transform.position.x < -9f || transform.position.x > 9f)
+        {
+            Destroy(this.gameObject); // Destroy the enemy if it goes out of the horizontal bounds (x < -9 or x > 9)
+            // This ensures that enemies do not stay in the scene indefinitely
+        }
+    }
+}
